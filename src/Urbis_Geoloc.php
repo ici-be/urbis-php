6e3d800fe981f521f7228561215b2b94cd700970
ici-brussels <bruno.veyckemans@gmail.com>
2017-11-04 15:06:51 +0100

    Update Urbis_Geoloc.php

--- src/Urbis_Geoloc.php
+++ src/Urbis_Geoloc.php
@@ -3,8 +3,11 @@ class Urbis_Geoloc
 { 
   private function createJson(bool $structured = true, string $StreetName, ?string $StreetNumber = null, ?int $PostalCode = null, string $lang = "fr", int $crs = 31370)
   {
-      $request = "http://geoservices.irisnet.be/localization/Rest/Localize/getaddressesfields?json=%7B%22language%22%3A%22".$lang."%22%2C%22address%22%3A%7B%22street%22%3A%7B%22name%22%3A%22".urlencode($StreetName)."%22%2C%22postcode%22%3A%22".$PostalCode."%22%7D%2C%22number%22%3A%22".$StreetNumber."%22%7D%2C%22spatialReference%22%3A%22".$crs."%22%7D";
-      echo $request;
+      if($structured) { $request = "http://geoservices.irisnet.be/localization/Rest/Localize/getaddressesfields?json=%7B%22language%22%3A%22".$lang."%22%2C%22address%22%3A%7B%22street%22%3A%7B%22name%22%3A%22".urlencode($StreetName)."%22%2C%22postcode%22%3A%22".$PostalCode."%22%7D%2C%22number%22%3A%22".$StreetNumber."%22%7D%2C%22spatialReference%22%3A%22".$crs."%22%7D"; }
+      else {
+        $request = "http://geoservices.irisnet.be/localization/Rest/Localize/getaddresses?language=".$lang."&address=".urlencode($StreetName)."&spatialReference=".$crs;
+      };
+
       return $request;
   }
 
@@ -23,9 +26,25 @@ class Urbis_Geoloc
       $this->_address = $this->doRequest($request);
   }
 
+  public function getAddress_Unstructured(string $Address)
+  {
+      $request        = $this->createJson(false, $Address);
+      $this->_address = $this->doRequest($request);
+  }
+
   public function getAllAddress()
   {
       return $this->_address??null;
+  }
+
+   public function getStructuredAddress()
+  {
+          $data['StreetName'] = $this->_address->result[0]->address->street->name;
+          $data['StreetNumber'] = $this->_address->result[0]->address->number;
+          $data['PostalCode'] = $this->_address->result[0]->address->street->postCode;
+          $data['MunicipalityName'] = $this->_address->result[0]->address->street->municipality;     
+
+          return $data;
   }
 
    public function getGeographicalLocation()
@@ -36,5 +55,5 @@ class Urbis_Geoloc
          return $data;
   }
 
-}  
+}
 ?>
